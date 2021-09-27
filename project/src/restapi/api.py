efda91d106bb8c9975ceff0b94ca26497e0ce16b
Volkov Nikolai <volkov@Volkov.local>
2021-09-27 21:44:52 +0300

    added list files

--- project/src/restapi/api.py
+++ project/src/restapi/api.py
@@ -1,8 +1,9 @@
 from restapi import app
 from flask_restful import Api
-from restapi.resources import HelloWorld, ListBuckets
+from restapi.resources import HelloWorld, ListBuckets, ListFiles
 
 
 api = Api(app)
 api.add_resource(HelloWorld, "/api")
 api.add_resource(ListBuckets, "/api/buckets")
+api.add_resource(ListFiles, "/api/<string:bucketname>/<string:username>")
--- project/src/restapi/resources.py
+++ project/src/restapi/resources.py
@@ -1,3 +1,4 @@
+import boto3
 from flask import jsonify
 from flask_restful import Resource
 from restapi.bucket import get_bucket, get_buckets_list
@@ -12,4 +13,12 @@ class ListBuckets(Resource):
     def get(self):
         response = jsonify(get_buckets_list())
         response.status_code = 200
+        return response
+
+
+class ListFiles(Resource):
+    def get(self, bucketname, username):
+        my_bucket = boto3.resource("s3").Bucket(bucketname)
+        all_files = list(my_bucket.objects.filter(Prefix=username))
+        response = jsonify([object.key for object in all_files])
         return response
