9241d11470f3300353cef9cb21d6496259321047
Volkov Nikolai <volkov@Volkov.local>
2021-09-28 21:35:31 +0300

    added tests

--- project/tests/test_restapi.py
+++ project/tests/test_restapi.py
@@ -1,5 +1,40 @@
 from restapi import __version__
+from restapi import app
+import unittest
+from io import BytesIO
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+class BasicTestCase(unittest.TestCase):
+    def test_home(self):
+        tester = app.test_client(self)
+        response = tester.get("/api", content_type="html/text")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'{\n    "hello": "world"\n}\n')
+
+    def test_buckets(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/buckets", content_type="html/text")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b"bucket" in response.data)
+
+    def test_write_access(self):
+        app_client = app.test_client()
+        response = app_client.post(
+            "/api/upload",
+            buffered=True,
+            content_type="multipart/form-data",
+            data={
+                "bucket": "volkovsssbucket",
+                "user": "TEST",
+                "file": (BytesIO(b"test contents"), "test.jpg"),
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
